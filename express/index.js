112c0ec2d141adfb97f966d514d82973449f1648
adiba <adibaapconic@gmail.com>
2023-10-25 15:39:11 +0530

    updated files

--- express/index.js
+++ express/index.js
@@ -9,34 +9,34 @@ app.listen(port, () => {
   console.log("app is listning on port 8080");
 });
 
-// app.use((req, res) => {
-//   //console.log(req);
-//   console.log("req received");
-//   res.send({
-//     name: "apple",
-//     color: "red",
-//   });
-// });
+app.use((req, res) => {
+  //console.log(req);
+  console.log("req received");
+  res.send({
+    name: "apple",
+    color: "red",
+  });
+});
 
 // routing
 app.get("/", (req, res) => {
   res.send("root path");
 });
 
-// app.get("/second", (req, res) => {
-//   res.send("u contected second path");
-// });
+app.get("/second", (req, res) => {
+  res.send("u contected second path");
+});
 
-// app.get("/next", (req, res) => {
-//   res.send("u contected next path");
-// });
-// app.get("*", (req, res) => {
-//   res.send("not exit path");
-// });
+app.get("/next", (req, res) => {
+  res.send("u contected next path");
+});
+app.get("*", (req, res) => {
+  res.send("not exit path");
+});
 
-// app.post("/", (req, res) => {
-//   res.send("you sent a post request to root");
-// });
+app.post("/", (req, res) => {
+  res.send("you sent a post request to root");
+});
 
 //path params
 app.get("/:username/:id", (req, res) => {
